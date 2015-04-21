19cf6cb74d042acb45f244e7b819974ea49670c2
Tyler Wiegand <tyler.wiegand@me.com>
2015-04-20 18:08:15 -0600

    Added codewizard.css, milestone-1.php, and .gitignore.

---
+++ documentation/milestone-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="stylesheet" type="text/css" href="../css/codewizard.css">
+		<title>thecodewizard milestone 1</title>
+	</head>
+	<body>
+		<h1>My Plan for World Domination</h1>
+		<h3>Purpose:</h3>
+		<ul>
+				<li>To produce and propagate <span="codewizard">thecodewizard</span></li>
+		<h3>Audience:</h3>
+		<ul>
+			<li>Potential Clients/Employers, Developers, Coding Apprentices</li>
+			<li>Developers</li>
+			<li>Code Wizard Apprentices</li>
+		</ul>
+		<h3>Goals:</h3>
+			<ul>
+				<li>Weave a personal web...site full of my skillz</li>
+				<li>Display to potential employers personal competency and drive</li>
+				<li>Springboard for ideas fleshed out online</li>
+			</ul>
+	</body>
+</html>
