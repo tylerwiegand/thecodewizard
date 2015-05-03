6979cb0fa38c9517d4a6a3704a4deb63c0009b21
Tyler Wiegand <tyler.wiegand@me.com>
2015-05-03 11:07:51 -0600

    Added sitemap.jpg, wizard2.jpg, uploaded to bcc instead of asmallorange because my site IS DOWN!! WTF! Also added milestone-2.php.

---
+++ documentation/milestone-2.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="../css/codewizard.css">
+		<title>thecodewizard milestone 1</title>
+	</head>
+	<body>
+		<h1>Wizard Mile(sword-in-the)stone Two</h1>
+		<img src="../img/wizard2.jpg" />
+		<h2>Introduction</h2>
+		<p>
+			This page is to paint a metaphorical picture of what a super
+			awesome wizarding themed "codefolio" (oo thats mine) should look
+			and/or feel like. The big idea here is that there are plenty of
+			obvious references to wizards and code. The intended audience is awesome
+			people, employers, clients, students, wizards. The main goal of people
+			that COME here should be to give me fat stacks of cash, wizard gold,
+			or a letter to Hogwarts. The secondary goal is for people to widen
+			their eyes in awizardment as they start to doubt their mortality for
+			incredible sights their eyes have begun to comprehend.
+		</p>
+		<h2>Persona</h2>
+		<p>
+			Name: Ivanna Castalot<br />
+			Age: 27
+		</p>
+		<p>
+			Profession: Founder of Wizarding Weekly, Ivanna is a busy witch
+			with a lot of time on her hands. Ivanna needs to convert her
+			weekly publication into a wizard-friendly interwebs place. She also
+			needs to make sure the proper charms are placed on the interweb place
+			so that muggles cannot access the site. But who should she floo powder?
+		</p>
+		<p>
+			Technology: Wand. No, but really, she has a custom built PC made by
+			Arthur Weasley. And a wand.
+		</p>
+		<p>
+			Attitudes and Behaviors: Ivanna spends all her time online now. She doesn't
+			have a life. Her Gringott's vault overfloweth, and the only thing that drives
+			her decision making process is, "How can I make the world a better place?"
+		</p>
+		<p>
+			Frustrations and Needs: Ivanna wants to throw as much gold as she can
+			at a moderately experienced young interwebs page developer. Ivanna secretly
+			loves muggles, and will need to keep this project a secret from her investors.
+			She hates poorly designed interweb pages, especially ones with no references
+			to wizarding in general.
+		</p>
+		<p>
+			Goals: Ivanna desperately needs to hire someone on, but secrecy is key. This muggle
+			should have some sort of clearance to work on delicate projects. Ivanna spends
+			hours researching people on monster.com. It's exhausting. But it's worth it.
+		</p>
+		<p>
+			Use Case
+			Ivanna Castalot has recently attended a soiree for networking and professional
+			development. When she woke up the next morning, she found a business card slid
+			deftly beneath her clothes. Impressed by this ambitious individual, Ivanna carefully
+			reads the contact info and immediately, with haste and anticipation, navigates
+			to the interweb page.
+		</p>
+		<p>
+			After many hours of researching the proprietor of "thecodewizard.net" on her 27" screen,
+			beastly PC Gaming Rig (courtesy Mr. Weasley) and (sadly) coming to the realization that
+			the owner was NOT a wizard in fact, but in enthusiasm, Ivanna comes to the conclusion
+			that this is the only muggle for her. Professionally.
+		</p>
+		<p>
+			Ivanna fluidly navigated through the below illustration, conceptually at least...
+		</p>
+		<img src="../img/sitemap.jpg" />
+		<p>
+			Minute One: Ivanna is blown away by the animated wizard appearance similar to
+			Mickey Mouse from the Mickey Mouse Club. Amazeballs.
+		</p>
+		<p>
+			Minute Two: Ivanna scrolls up and down and clicks on Ellas super dope bejewelled game.
+		</p>
+		<p>
+			Minute Three Hundred: Ivanna reads a few articles that fade into contrast letter by letter as you
+			mouse over the box containing them. She mouses over the pictures that seem to move, similar
+			pictures in her world. Breathtaking. Ivanna sees the email at the bottom (same as the business
+			card), and clicks it to open a form to email our Code Wizard.
+		</p>
+		<h2>Proposed Site Map</h2>
+		<p>
+			One Page, many things on page, such as:
+		</p>
+		<ul>
+			<li>THECODEWIZARD.NET title page</li>
+			<li>Articles of success stories</li>
+			<li>Pictures of people working, playing, and casting</li>
+			<li>Contact Info</li>
+			<li>Mom</li>
+		</ul>
+		<p>
+			<strong>Title Page</strong> will consist of the mickey mouse wizard popping out of a circle. His eyes
+			will shift around all shifty.
+		</p>
+		<p>
+			<strong>Quotes</strong> of success story articles (clicking the article excerpts will fill in the blank
+			spaces on the front and back as a popup that seamlessly blends in with the original text).
+		</p>
+		<p>
+			<strong>Pictures</strong> of people working playing and casting, which is the site's motto. Casting is
+			intended to be euphemistic for coding. That will be understood from the picture of the
+			dude coding with a gigantic caption of CAST.
+		</p>
+		<p>
+			My <strong>contact info</strong> will appear upon clicking "About Me." THEIR contact info will pop up
+			upon clickage of "About You," and will freak them out. That's right, freak them out.
+		</p>
+		<p>
+			<strong>Awesome Stuff</strong> will pop up a simple tile view of other pages I have created (capstone?).
+		</p>
+		<p>
+			<strong>Mom</strong> will pop up a rounded-off-edges white bubble talk-box lookin thing with a pic
+			of my mom. How sweet.
+		</p>
+		<p>
+			Obligatory google adsense implemented as tasteless as possible. Willing to negotiate
+			this implementation.
+		</p>
+		<h2>Proposed Features and Functionality</h2>
+		<ul>
+			<li>Clicking links will either pop up a bubbley white dialogue lookin thingy OR scroll up or down to content.</li>
+			<li>Mousing over text will create a sheen effect to highlight the excerpt.</li>
+			<li>Mousing over images will slowly begin their subtle animation.</li>
+			<li>Clicking on the wizard will open a little talk bubble next to his head with a form to email me.</li>
+			<li>Wizards on broomsticks will fly by in a cartoonish fashion in the background.</li>
+		</ul>
+		<h2>Stretchy Goals</h2>
+		<ul>
+			<li>Background will change with time of day.</li>
+			<li>Theme will shift in reaction to time of day to be readable.</li>
+			<li>Any animation is considered a stretch goal.</li>
+		</ul>
+	</body>
+</html>
