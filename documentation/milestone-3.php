7f1b72129f094accc3e7947c148f2c23477d5d8d
Tyler Wiegand <tyler.wiegand@me.com>
2015-05-13 16:51:21 -0600

    Added milestone-3.php

---
+++ documentation/milestone-3.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="../css/codewizard.css">
+		<title>thecodewizard milestone 1</title>
+	</head>
+	<body>
+		<h1>Mile at Rock 3</h1>
+		<h2>Mobile Layout</h2>
+			<img src="../img/codewizardmobile.jpg" alt="CodeWizard Mobile Wireframe" />
+			<p>
+				So the general idea here is that the user will scroll down or click on the CTA
+				to select the desired section (which are few at this point, but can expand). Every
+				option won't be layed out on the initial visible space, but the two most important
+				ones will. As the user scrolls down, sections will expand. The code sample
+				will have the code on one side that fades out as it gets closer to the bottom
+				of the page, and the result of the code will be formatted similiarly on the right side
+				of the page.
+			</p>
+			<p>
+				On the bottom, contact me and mom will pop up into the shown windows and will not have separate
+				pages dedicated to them. The about me is its own section, and just moves you upward to the
+				<code>#spot.</code>
+			</p>
+		<h2>Desktop Layout</h2>
+		<img src="../img/codewizarddesktop.jpg" alt="CodeWizard Desktop Wireframe" />
+			<p>
+				Desktop version will be another affair entirely -- the screen will scroll to the right instead of down
+				with a playable character on the screen for the user to playfully navigate through the site. The playable
+				wizard will go up a ladder, fall into a lake, crawl out and end up at Mom toward the end.
+			</p>
+			<p>
+				Along his journey, the wizards presence will expand the sections available. However, if the user
+				indicates they do NOT want to be taken on a magical journey, the wizard sits down disheartened and
+				just looks annoyed while scrolling down goes right and the page reacts to movements of the user instead.
+				OF COURSE IT'S A STRETCH GOAL!
+			</p>
+	</body>
+</html>
