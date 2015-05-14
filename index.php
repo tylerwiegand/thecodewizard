2e206c280c41536e0e321274fdb4745ff7d1b1f4
Tyler Wiegand <tyler.wiegand@me.com>
2015-05-14 15:48:20 -0600

    testing bootstrap

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+		<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.4/superhero/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/codewizard.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<title>wizards winsome weblies</title>
+	</head>
+	<body>
+		<h1>Ipsum Start</h1>
+			<p>
+				Ya auta yeste' aa' lasser en lle coia orn n' omenta gurtha amin delotha lle
+				'Ksherea. Elear Asca nai turamin lle lakwenien? Amin caela ie'lle Nwalmaerea
+				maien quel n'quel. Edan rima ten'ta! quel undome tanya awra!. Hodoerea lle
+				ista amin quella no' amin Aierea amin uuma merna ta. Detholalle ron rangwa
+				amin irma haeannon manke naa nae lle llie lye tuulo'?
+			</p>
+			<p>
+				Yala onna lova handasse sana termara en' templa Ianterea. A! mani naa ta
+				N'hrive faina templa. Mani er ama poldora Aierea aa' menle nauva calen ar'
+				ta hwesta e' ale'quenle. Ai' atar Qualohtarie lle wethrine amin Manke?
+				Ndengina ta ho Hallaerea Arwenamin Aaye. Tincya en' russe tuulo' moriloomir
+				Harnram Tel'Domeduathea elea i'dolen.
+			</p>
